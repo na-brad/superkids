4d58fcae696d34a51fd69ec8f79dd2e9a76c927a
Brad Na <na.hyungjun@gmail.com>
2018-09-13 05:41:19 -0400

    added achievements

--- client/src/components/dashboard/Achievement.js
+++ client/src/components/dashboard/Achievement.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import Moment from 'react-moment';
-import { deleteExperience } from '../../actions/profileActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import { deleteAchievement } from "../../actions/profileActions";
 
-class Experience extends Component {
+class Achievement extends Component {
   onDeleteClick(id) {
-    this.props.deleteExperience(id);
+    this.props.deleteAchievement(id);
   }
 
   render() {
-    const experience = this.props.experience.map(exp => (
+    const achievement = this.props.achievement.map(exp => (
       <tr key={exp._id}>
         <td>{exp.company}</td>
         <td>{exp.title}</td>
         <td>
-          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment>
+          {/* -
           {exp.to === null ? (
-            ' Now'
+            " Now"
           ) : (
             <Moment format="YYYY/MM/DD">{exp.to}</Moment>
-          )}
+          )} */}
         </td>
         <td>
           <button
@@ -34,16 +35,16 @@ class Experience extends Component {
     ));
     return (
       <div>
-        <h4 className="mb-4">Experience Credentials</h4>
+        <h4 className="mb-4">Achievement Credentials</h4>
         <table className="table">
           <thead>
             <tr>
               <th>Company</th>
               <th>Title</th>
-              <th>Years</th>
+              <th>Year</th>
               <th />
             </tr>
-            {experience}
+            {achievement}
           </thead>
         </table>
       </div>
@@ -51,8 +52,11 @@ class Experience extends Component {
   }
 }
 
-Experience.propTypes = {
-  deleteExperience: PropTypes.func.isRequired
+Achievement.propTypes = {
+  deleteAchievement: PropTypes.func.isRequired
 };
 
-export default connect(null, { deleteExperience })(Experience);
+export default connect(
+  null,
+  { deleteAchievement }
+)(Achievement);
--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ProfileActions = () => {
   return (
@@ -7,9 +7,9 @@ const ProfileActions = () => {
       <Link to="/edit-profile" className="btn btn-light">
         <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
       </Link>
-      <Link to="/add-experience" className="btn btn-light">
+      <Link to="/add-achievement" className="btn btn-light">
         <i className="fab fa-black-tie text-info mr-1" />
-        Add Experience
+        Add Achievement
       </Link>
       <Link to="/add-education" className="btn btn-light">
         <i className="fas fa-graduation-cap text-info mr-1" />
