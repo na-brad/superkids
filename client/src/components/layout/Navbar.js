d00d46f23260e1ffa015b1dba7ff169406723dda
Daniel Kim <kim.daniel24@gmail.com>
2018-09-13 06:37:11 -0400

    landing UI complete

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -54,7 +54,7 @@ class Navbar extends Component {
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/login">
+          <Link className="nav-link nav-log" to="/login">
             Login
           </Link>
         </li>
@@ -62,9 +62,9 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-lg mb-4">
         <div className="container">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand logo" to="/">
             Super Kids
           </Link>
           <button
@@ -79,10 +79,10 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/profiles">
+                {/* <Link className="nav-link" to="/profiles">
                   {" "}
                   Developers
-                </Link>
+                </Link> */}
               </li>
             </ul>
             {isAuthenticated ? authLinks : guestLinks}
